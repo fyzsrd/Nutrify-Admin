75add775039bbe45f1cdcfccd240a9cf733789ee
Fayaz Sardar <fyz.srd@gmail.com>
2025-10-07 14:36:11 +0530

    feat: dashBoard

--- src/features/products/components/ProductModal.jsx
+++ src/features/products/components/ProductModal.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getPanelFormBrands, getpanelSubCategory } from '../../../api/formApi'
 
-const AddProductModal = ({ isOpen, onClose, onSave, getFormData }) => {
+const AddProductModal = ({ isOpen, onClose, onSave,loading }) => {
   const [formData, setFormData] = useState({
     name: "",
     brand: "",
@@ -92,6 +92,9 @@ const AddProductModal = ({ isOpen, onClose, onSave, getFormData }) => {
   }
 
   onSave(data);
+
+  
+
   
 };
 
@@ -323,10 +326,33 @@ const AddProductModal = ({ isOpen, onClose, onSave, getFormData }) => {
               Cancel
             </button>
             <button
+            disabled={loading}
               type="submit"
               className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700"
             >
-              Save
+              {loading && (
+                <svg
+                  className="animate-spin h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8v4l-3 3 3 3v4a8 8 0 01-8-8z"
+                  ></path>
+                </svg>
+              )}
+            {loading ? 'Saving' : "Save"}
             </button>
           </div>
         </form>
